1c53cc349ca0a0e593f22cc94c1c78410a2118a3
Manuperex <manuperex24@gmail.com>
2023-02-22 16:07:29 -0500

    Change in env

--- src/database/connection.js
+++ src/database/connection.js
@@ -10,20 +10,3 @@ objectdb.on ('error', () => {console.log('Error en la conexion a MongoDB')});
 
 
 module.exports = mongoose
-
-// mongoose.set('strictQuery', true);
-// const dbConnect = ()=>{
-//     const DB_URI = process.env.DB_URI //Se conecta a la url
-//     mongoose.connect(DB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//     }, (err, res)=>{
-//         if(!err){
-//           console.log('***CONEXION CORRECTA***')
-//         }else{
-//           console.log('***ERROR DE CONEXION***')
-//         }
-//     })
-
-// }
-// module.exports = { dbConnect }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const v1WorkoutRouter = require('./v1/routes/workoutRoutes')
 require('dotenv').config()
 
-// const { dbConnect } = require('./database/connection')
-// dbConnect()
 const archivoBD = require('./database/connection')
 const app = express();
 
@@ -18,22 +16,3 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`) });
-// require('dotenv').config()//variables de entorno
-// const express = require('express')
-// const cors = require('cors')
-// const app = express()
-// const PORT = process.env.PORT || 3000 //puerto declarado en la variable de entorno
-// const { dbConnect } = require('./database/connection') //requiere la conexion
-
-
-
-
-// app.use(cors())
-// app.use(express.json()) //permite envie de datos
-
-// app.use('/api/v1/workouts', require('./v1/routes/workoutRoutes')) //ruta de la api
-
-// dbConnect()
-// app.listen(PORT, ()=>{ //Api escuchando
-//   console.log('api lista por el puerto', PORT)
-// })
